21ffb90cbe1bdc0688cec9e6e579a9dd036ff94e
Ozberk Ercin <ozberk@breakthrough.health>
2018-08-16 16:52:47 +0200

    add js

---
+++ test.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const crawler = require('.');
+
+console.log(crawler.default('../../mvp'));
+// console.log(crawler.run());
+// console.log(crawler());
+// crawler('../../mvp');

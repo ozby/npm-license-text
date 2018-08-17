647d606f89128b09f009a534edbad771711b8f60
Ozberk Ercin <ozberk@breakthrough.health>
2018-08-17 15:18:37 +0200

    exclude js files

--- test.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const crawler = require('.');
-console.log('+++');
-crawler.default('../../mvp', 'b.json').then((data) => {console.log('test'); console.log(data)});
-console.log('---');
-// console.log(crawler.run());
-// console.log(crawler());
-// crawler('../../mvp');
